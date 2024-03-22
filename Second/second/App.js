019b80534090445cf293d5c5808b91d9a282ff12
Prabhat Ranjan Shahi <ranjanprabhat555@gmail.com>
2024-03-22 15:30:31 +0530

    basic structure

---
+++ Second/second/App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text style={{fontSize: 30}}>Hello React Native</Text>
+      <Text style={{fontSize: 30}}>Prabhat</Text>
+      <Text style={{fontSize: 30}}>Ranjan Shahi</Text>
+    </View>
+  );
+}
+
+export default App;
